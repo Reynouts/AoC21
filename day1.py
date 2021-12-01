8f639da971fa735523ca912a83401fe2f2529dcb
Reinout <rcw.versteeg@avans.nl>
2021-12-01 09:16:28 +0100

    List comprehensioning day 1

--- day1.py
+++ day1.py
@@ -1,9 +1,13 @@
-from collections import deque as dq
-import re
+def solve(data: list[int], window: int = 1) -> int:
+    return sum([1 for index, n in enumerate(data[window:], start=window) if data[index - window] < n])
+
 
 def main():
     with open('day1.txt', 'r') as f:
-        data = f.read()
+        data = list(map(int, f.read().split()))
+    print(f"Part 1: {solve(data)}")
+    print(f"Part 2: {solve(data, 3)}")
+
 
 if __name__ == "__main__":
     main()
